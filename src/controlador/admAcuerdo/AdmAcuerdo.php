e9a77f367eb223799fbc2904017a171cead3f2fb
RenzoC <renzocallachavez@gmail.com>
2019-10-30 15:09:51 -0400

    Creacion de criterios de origen, aranceles, acuerdos

--- src/controlador/admAcuerdo/AdmAcuerdo.php
+++ src/controlador/admAcuerdo/AdmAcuerdo.php
@@ -69,9 +69,9 @@ class AdmAcuerdo extends Principal {
 
     if($_REQUEST['tarea'] == 'saveAcuerdo'){
             $today = date("Y-m-d h:m:s");
-            $esUsado = $this->checkAcuerdo($_REQUEST['id_acuerdo']);
+            $noesUsado = $this->checkAcuerdo($_REQUEST['id_acuerdo']);
 
-            if($_REQUEST['id_acuerdo'] && !$esUsado){
+            if($_REQUEST['id_acuerdo'] && $noesUsado){
                 $acuerdo->setId_Acuerdo($_REQUEST['id_acuerdo']);
                 $acuerdo = $sqlAcuerdo->getBuscarAcuerdoPorId($acuerdo);
             }
@@ -125,7 +125,7 @@ class AdmAcuerdo extends Principal {
                     }
                 }
                 //anular acuerdo si es edicion
-                if($_REQUEST['id_acuerdo'] && $esUsado) $this->anularAcuerdo($_REQUEST['id_acuerdo']);
+                if($_REQUEST['id_acuerdo'] && !$noesUsado) $this->anularAcuerdo($_REQUEST['id_acuerdo']);
 
                 echo '{"status":"1","message":"El acuerdo se cuardo satisfactoriamente."}';
             }
