4f9c602c1c5d8e0d78f6a4ac1ec06b5eb1f7ad4d
RenzoC <renzocallachavez@gmail.com>
2019-10-30 15:21:29 -0400

    Creacion de criterios de origen, aranceles, acuerdos

--- src/tabla/RubroExportaciones.php
+++ src/tabla/RubroExportaciones.php
@@ -15,6 +15,7 @@ class RubroExportaciones extends Db {
 
     private $id_rubro_exportaciones;
     private $descripcion;
+    private $criterio_valor;
     
     public function setId_rubro_exportaciones($id_rubro_exportaciones) {
         $this->id_rubro_exportaciones = $id_rubro_exportaciones;
@@ -28,6 +29,12 @@ class RubroExportaciones extends Db {
     }
     public function getDescripcion() {
         return $this->descripcion;
+    }
+    public function getCriterio_valor(){
+        return $this->criterio_valor;
+    }
+    function setCriterio_valor($criterio_valor){
+        $this->criterio_valor=$criterio_valor;
     }
     
 }
